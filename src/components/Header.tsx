44693cb05b85ced90848293618b62d48ed95401d
Pacify01 <124194931+Pacify01@users.noreply.github.com>
2023-05-08 17:47:40 +0100

    Clearing code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 
+
 type Props = {};
 
 function Header({}: Props) {
@@ -22,32 +23,13 @@ function Header({}: Props) {
           duration: 1.5,
         }}
         className="flex flex-row items-center "
-        
-      ><img
-      className="relative h-24 w-24 xl:h-32 xl:w-32 mx-auto object-cover z-50 opacity-0 md:opacity-100"
-      src="https://res.cloudinary.com/ddsmswmsz/image/upload/v1683562759/SIGNING-4_kasyjg.png"
-      alt=""></img>
-        {/* Social Icons */}
-        
+      >
+        <img
+          className="relative h-24 w-24 xl:h-32 xl:w-32 mx-auto object-cover z-50 opacity-0 md:opacity-100"
+          src="https://res.cloudinary.com/ddsmswmsz/image/upload/v1683562759/SIGNING-4_kasyjg.png"
+          alt=""
+        ></img>
       </motion.div>
-      <motion.div
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 1.5,
-        }}
-        className="flex flex-row items-center text-blue-300 cursor-pointer"
-        onClick={() => (window.location.href = "#contact")}
-        
-      ></motion.div>
     </header>
   );
 }
